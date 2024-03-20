af114b3bdcd459492ab7e1adc2656b07e2156788
VladislavAzamat <159619739+VladislavAzamat@users.noreply.github.com>
2024-03-20 17:24:43 +0300

    TransferTimeFilter.java

--- src/main/java/com/gridnine/testing/filters/TransferTimeFilter.java
+++ src/main/java/com/gridnine/testing/filters/TransferTimeFilter.java
@@ -21,8 +21,6 @@ public class TransferTimeFilter implements Filter{
 
     @Override
     public List<Flight> doFilter(List<Flight> flights) {
-        //добавляем список полетов и в результате возвращается список отфильтрованный
-        //в зависимости от того сколько сегментов было в каждом полете, есть 4 варианта
         switch (type) {
             case LESS -> {
                 return flights.stream().filter(f -> compareTransferTimeWithTarget(f.getSegments()) <= 0).toList();
@@ -40,10 +38,10 @@ public class TransferTimeFilter implements Filter{
     }
 
     private int compareTransferTimeWithTarget(List<Segment> segments) { //
-        if(segments.size() == 1) return -1; // если один сегмент - возвращаем
-        var sortedSegments = FilterUtils.getSegmentsSorted(segments); // сортируем сегменты
-        Duration timeInTransfer = Duration.of(0, ChronoUnit.HOURS);// какой то непонятных дурейшен, типо инициализация
-        for(int i = 0; i < sortedSegments.size() - 1; i++) { //  цикл по сегментам, плюсуем в тайм трансфер и сравниваем с дурейшн который при инициализации метода запускается
+        if(segments.size() == 1) return -1; 
+        var sortedSegments = FilterUtils.getSegmentsSorted(segments); 
+        Duration timeInTransfer = Duration.of(0, ChronoUnit.HOURS);
+        for(int i = 0; i < sortedSegments.size() - 1; i++) {
             timeInTransfer = timeInTransfer.plus(Duration.between(
                     sortedSegments.get(i).getArrivalDate(),
                     sortedSegments.get(i + 1).getDepartureDate())
